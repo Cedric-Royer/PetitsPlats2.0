1e079263fd8d9af75f1bdc43afa960d330adc691
Cedric Royer <cedricroyerpro@gmail.com>
2024-11-12 21:27:27 +0100

    clean code

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -1,26 +1,21 @@
 export function setupDropdownListeners(selectedTags, updateUI) {
-    // Gestion des clics sur les dropdowns
     document.addEventListener('click', event => {
         const dropdownToggle = event.target.closest('.dropdown-toggle');
         const dropdown = event.target.closest('.dropdown');
         const filterInput = event.target.closest('.dropdown-filter-input');
 
         if (dropdownToggle) {
-            // Ouvrir/Fermer le dropdown si le bouton toggle est cliqué
             toggleDropdown(dropdown);
         } else if (event.target.matches('.dropdown-item')) {
-            // Logique pour gérer la sélection des éléments dans le dropdown
             const filterType = dropdown.id.split('-')[0];
             handleDropdownSelection(event, selectedTags, filterType, updateUI);
             closeAllDropdowns();
         } else if (!dropdown || !filterInput) {
-            // Fermer tous les dropdowns si le clic est à l'extérieur et pas sur le champ de filtrage
             closeAllDropdowns();
         }
     });
 }
 
-// Fonction pour gérer l'ouverture et la fermeture du dropdown
 export function toggleDropdown(dropdown) {
     dropdown.classList.toggle('open');
 
@@ -50,7 +45,6 @@ export function toggleDropdown(dropdown) {
     }
 }
 
-// Fonction pour gérer la sélection d'éléments dans le dropdown
 export function handleDropdownSelection(event, selectedTags, filterType, updateUI) {
     const item = event.target;
 
@@ -59,19 +53,17 @@ export function handleDropdownSelection(event, selectedTags, filterType, updateU
 
         if (!selectedTags[filterType].includes(value)) {
             selectedTags[filterType].push(value);
-            updateUI(); // Met à jour l'interface utilisateur après la sélection
+            updateUI(); 
         }
     }
 }
 
-// Fonction pour fermer tous les dropdowns
 export function closeAllDropdowns() {
-    // Fermer tous les dropdowns en retirant la classe 'open'
     document.querySelectorAll('.dropdown').forEach(dropdown => {
         dropdown.classList.remove('open');
     });
     
-    // Sélectionner tous les boutons de dropdown
+
     document.querySelectorAll('.dropdown-toggle').forEach(dropdownToggle => {
         dropdownToggle.classList.add('mb-4', 'rounded-lg');
     });
